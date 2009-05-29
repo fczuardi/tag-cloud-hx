871f1369f3b65f09491dd35bd66c7df70969815c
Fabricio Zuardi <fabricio@gmail.com>
2009-05-29 18:41:23 -0300

    Friday updates

--- src/org/fabricio/tags/Tag.hx
+++ src/org/fabricio/tags/Tag.hx
@@ -1,34 +1,80 @@
+#
+# TagCould-hx
+#
+# A library for creating tag-clouds in haXe.
+#
+# http://github.com/fczuardi/tag-cloud-hx
+# 
+# Copyright (c) 2009 The TagCloud-hx Authors.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions 
+# are met:
+# 
+#   * Redistributions of source code must retain the above copyright 
+#     notice, this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in 
+#     the documentation and/or other materials provided with the 
+#     distribution.
+#   * Neither the name of the HSBC Brasil S.A., HSBC México S.A.,
+#     JWT Brasil S.A. nor the names of its contributors may be used to 
+#     endorse or promote products derived from this software without 
+#     specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 package org.fabricio.tags;
 
 import flash.text.Font;
 import flash.text.TextField;
 import flash.text.TextFieldAutoSize;
 import flash.text.TextFormat;
-import flash.display.MovieClip;
+import flash.display.Sprite;
 
 // ==== package org.fabricio.tags ====
 
 // = Tag =
 // A visual tag. An object representing an individual keyword.
-class Tag extends MovieClip{
+class Tag extends Sprite{
   
   // == Constants ==
   // The default values for various settings
   private static inline var DEFAULT_TEXT:String     = '';
   private static inline var DEFAULT_SIZE:Int        = 18;
   private static inline var DEFAULT_COLOR:Int       = 0x333333;
-  private static inline var DEFAULT_FONT:String     = "DejaVuSansCondensedBold";
+  private static inline var DEFAULT_FONT:String     = "_sans";
   private static inline var DEFAULT_EMBED_FONT:Bool = false;
   
   // == Properties ==
   
-  // === text:String ===
-  // The label of the tag.
-  public var text(getLabel, setLabel):String;
+  // === tagName:String ===
+  // The identifier of the tag.
+  public var tagName:String;
+
+  // === label:String ===
+  // The displayed text of the tag.
+  public var label(getLabel, setLabel):String;
   private function getLabel():String{
     return _label.text;
   }
   private function setLabel(text:String){
+    //if the tag does not have an identifier yet, setup it up
+    if (tagName == null) {
+      tagName = text;
+    }
+    //update the label
     _label.text = text;
     _label.setTextFormat(_format);
     return _label.text;
@@ -86,22 +132,47 @@ class Tag extends MovieClip{
   
   // == Constructor ==
   // Accepts the following optional parameters:
-  // * **{{{text}}}**  – the tag label to be displayed.
+  // * **{{{text_or_config}}}**  – the tag label to be displayed. 
+  // Or a config object containing all parameters: text, size, color, font, name.
   // * **{{{size}}}**  – the font size.
   // * **{{{color}}}** – the hexadecimal color — ex: {{{0x00FF00}}}.
   // * **{{{font}}}**  – the name of the font to use.
-  public function new(?text:String, ?size:Float, ?color:Int, ?font:String){
+  // * **{{{name}}}**  – the tagName to be used as an identifier.
+  //
+  // Usage Examples:
+  // {{{
+  //   var tagA = new Tag({
+  //     text  : "HAXE", 
+  //     size  : 30, 
+  //     color : 0x003333, 
+  //     font  : "DejaVuSansCondensedBold",
+  //     name  : "haXe"
+  //   });
+  //   
+  //   var tagB = new Tag('Rules', 30);
+  // }}}
+  public function new(?text_or_config:Dynamic, ?size:Float, 
+                               ?color:Int, ?font:String, ?name:String){
     super();
     _label            = new TextField();
     _format           = new TextFormat();
     _label.autoSize   = TextFieldAutoSize.LEFT;
     _label.embedFonts = DEFAULT_EMBED_FONT;
-    
-    _label.text       = (text == null)  ? DEFAULT_TEXT  : text;
+    var text:String   = null;
+    if (Reflect.isObject(text_or_config)){
+      text  = Reflect.field(text_or_config, 'text');
+      size  = Reflect.field(text_or_config, 'size');
+      color = Reflect.field(text_or_config, 'color');
+      font  = Reflect.field(text_or_config, 'font');
+      name  = Reflect.field(text_or_config, 'name');
+    } else {
+      text = text_or_config;
+    }
+    tagName           = name;
     _format.size      = (size == null)  ? DEFAULT_SIZE  : size;
     _format.color     = (color == null) ? DEFAULT_COLOR : color;
-    this.font      = (font == null)  ? DEFAULT_FONT  : font;
-    _label.setTextFormat(_format);
+    this.font         = (font == null)  ? DEFAULT_FONT  : font;
+    this.label        = (text == null)  ? DEFAULT_TEXT  : text;
     this.addChild(_label);
   }
   
--- src/org/fabricio/tags/TagCloud.hx
+++ src/org/fabricio/tags/TagCloud.hx
@@ -1,31 +1,141 @@
+#
+# TagCould-hx
+#
+# A library for creating tag-clouds in haXe.
+#
+# http://github.com/fczuardi/tag-cloud-hx
+# 
+# Copyright (c) 2009 The TagCloud-hx Authors.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions 
+# are met:
+# 
+#   * Redistributions of source code must retain the above copyright 
+#     notice, this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in 
+#     the documentation and/or other materials provided with the 
+#     distribution.
+#   * Neither the name of the HSBC Brasil S.A., HSBC México S.A.,
+#     JWT Brasil S.A. nor the names of its contributors may be used to 
+#     endorse or promote products derived from this software without 
+#     specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 package org.fabricio.tags;
-/*import org.fabricio.tags.TagList;*/
-import flash.display.MovieClip;
+
+import flash.display.Sprite;
+
 // ==== package org.fabricio.tags ====
 
 // = TagCloud =
 // A visualization of a tags set as a cloud.
-class TagCloud extends MovieClip{
+class TagCloud extends Sprite{
   
-  public function new(list:TagList){
-    super();
+
+  private static inline var DEFAULT_CAPITALIZATION:Capitalization = preserve;
+
+  // == Settings ==
+  // Attributes and preferences for the cloud.
+  public var capitalization(getCaps, setCaps):Capitalization;
+  private function getCaps():Capitalization{
+    return _config.capitalization;
+  }
+  private function setCaps(cap:Capitalization){
+    _config.capitalization = cap;
+    for (i in _tags){
+      i.label = applyCapitalization(i.tagName);
+    }
+    return _config.capitalization;
+  }
+  
+  private function applyCapitalization(s:String):String{
+    switch (_config.capitalization){
+      case preserve:
+        return s;
+      case lower:
+        return s.toLowerCase();
+      case upper:
+        return s.toUpperCase();
+      case capitalized:
+        return Lambda.list(s.split(' ')).map(capitalize).join(' ');
+    }
+  }
+  
+  private function capitalize(s:String):String { 
+    return  s.charAt(0).toUpperCase() + s.toLowerCase().substr(1,s.length);
+  }
+  
+  // generate all the tag objects of the cloud
+  private function create():Void{
     var x:Float = 0;
     var y:Float = 0;
-    var tags:Array<Tag> = [];
-    for (i in list){
+    for (i in _tagList){
       if(i.value>3){
-        var tag = new Tag(i.name.toUpperCase(), i.value *1.2+4, 0x333333, "DejaVuSansCondensedBold");
+        var tag = new Tag({
+          text  : applyCapitalization(i.name), 
+          size  : i.value *1.2+4, 
+          color : 0x003333, 
+          font  : "DejaVuSansCondensedBold",
+          name  : i.name
+        });
         tag.alpha = 0.8;
         tag.x = x;
         tag.y = y - (tag.height)/2;
-        this.addChild(tag);
+        _tags.push(tag);
+        addChild(tag);
         x += tag.width;
         if(x>350){
           x = Math.random()*20;
           y += 30;
         }
       }
+
     }
-    
   }
+  
+  // == Constructor ==
+  // Acceps the following optional parameters:
+  // * **{{{list}}}** a [[#src/org/fabricio/tags/TagList.hx | Taglist]] object.
+  public function new(?list:TagList){
+    super();
+    _tags = [];
+    _config = {
+      capitalization : DEFAULT_CAPITALIZATION
+    }
+    _tagList = (list == null) ? new TagList() : list;
+    if (list != null) {
+      create();
+    }
+  }
+  
+  private var _tagList:TagList;
+  private var _tags:Array<Tag>;
+  private var _config:TagCloudConfig;
 }
+
+enum Capitalization{
+  preserve;
+  lower;
+  upper;
+  capitalized;
+}
+
+typedef TagCloudConfig = {
+  var capitalization:Capitalization;
+}
+
+
